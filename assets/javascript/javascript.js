d686080fd4089c9fd123928ba2f466aaec2ab18a
Alex Kim <askim1019@gmail.com>
2018-05-05 13:15:48 -0500

    Create skeletal strucuture of project

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,14 @@
 console.log("hello");
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAM4Op2cHGkCwqhOhsnGJ-S4IkTQD5w-3U",
+    authDomain: "datingapp-f81f6.firebaseapp.com",
+    databaseURL: "https://datingapp-f81f6.firebaseio.com",
+    projectId: "datingapp-f81f6",
+    storageBucket: "datingapp-f81f6.appspot.com",
+    messagingSenderId: "863725689814"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database().ref();
